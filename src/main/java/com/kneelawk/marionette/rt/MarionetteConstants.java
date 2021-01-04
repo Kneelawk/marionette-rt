94b26b24bf6038549f1f5e0905734ce9d033922c
Kneelawk <kneelawk@gmail.com>
2021-01-03 17:19:18 -0800

    Add marionette version tracker

--- src/main/java/com/kneelawk/marionette/rt/MarionetteConstants.java
+++ src/main/java/com/kneelawk/marionette/rt/MarionetteConstants.java
@@ -1,7 +1,22 @@
 package com.kneelawk.marionette.rt;
 
-public interface MarionetteConstants {
-    String ENVIRONMENT_PROPERTY = "com.kneelawk.marionette.env";
-    String INSTANCE_NAME_PROPERTY = "com.kneelawk.marionette.instance_name";
-    String RMI_PORT_PROPERTY = "com.kneelawk.marionette.rmi_port";
+import java.io.IOException;
+import java.util.Properties;
+
+public class MarionetteConstants {
+    public static final String ENVIRONMENT_PROPERTY = "com.kneelawk.marionette.env";
+    public static final String INSTANCE_NAME_PROPERTY = "com.kneelawk.marionette.instance_name";
+    public static final String RMI_PORT_PROPERTY = "com.kneelawk.marionette.rmi_port";
+
+    public static final String MARIONETTE_VERSION;
+
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(MarionetteConstants.class.getResourceAsStream("marionette-rt.properties"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to load marionette-rt.properties", e);
+        }
+        MARIONETTE_VERSION = properties.getProperty("marionette_version");
+    }
 }
